f54ed1750e1273382f3475597c336e48014fd217
Yuji Sasaki <jyu0414@gmail.com>
2020-01-08 10:38:52 +0900

    get tweet

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import posttweet
 
 if __name__ == '__main__':
     #メンションされたツイートを取得
+    gettweet.getTweet()
     mentionedTweets = ["日本政府が韓国向け輸出管理厳格化の対象にしている「フッ化水素」。韓国の産業通商資源省は、韓国の化学メーカーが不純物"]
     
     
