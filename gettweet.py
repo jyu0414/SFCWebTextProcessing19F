b344e88e870d4e80c8ae5a667b9c03167d5b55bd
Yuji Sasaki <jyu0414@gmail.com>
2020-01-11 15:42:55 +0900

    test

--- gettweet.py
+++ gettweet.py
@@ -6,24 +6,27 @@ import json
 
 def getTweet():
     
-    getTweetTextsFor("Sankei_news")
-    getTweetTextsFor("")
+    tweets = getTweetTextsFor("Sankei_news")
+    #tweets.extend(getTweetTextsFor("asahi"))
+    #tweets.extend(getTweetTextsFor("Yomiuri_Online"))
 
-    return ["日本政府が韓国向け輸出管理厳格化の対象にしている「フッ化水素」。韓国の産業通商資源省は、韓国の化学メーカーが不純物を「1兆分の1」まで抑え、大量生産が可能な製造技術を確立したと発表しました。",
-    "日産元会長、カルロス・ゴーン被告の海外逃亡で、東京地検が東京都港区の住居を出入国管理法違反の疑いで家宅捜索。レバノン政府は逃亡直前の12月20日、日本の外務副大臣にゴーン被告の送還を要求していました。",
-    "「個人的な問題であり、分からない」。カルロス・ゴーン被告が入国する経緯への関与を否定したレバノン政府。入国は「合法的」としており、日本側が身柄引き渡し要請した場合に協力を得られるかは不透明です。"]
+    return getRelatedTweets(tweets)
 
 def getTweetTextsFor(name):
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={0}&count=10".format(name)
-
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=Sankei_news&count=5"#.format(name)
+    print(url)
     payload = {}
     headers = {
-        'Authorization': 'OAuth oauth_consumer_key="JolGq4w2PqMWCfDr4szMLcupZ",oauth_token="1214413074179866625-XZsV1CVPd72h5wQ56zjNAMWycLKCA8",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1578446430",oauth_nonce="TAbT3sy6Q39",oauth_version="1.0",oauth_signature="v6dUZkCTJkRFye7k6SuxQWRmE5w%3D"'
+        'Authorization': 'OAuth oauth_consumer_key="JolGq4w2PqMWCfDr4szMLcupZ",oauth_token="1214413074179866625-XZsV1CVPd72h5wQ56zjNAMWycLKCA8",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1578448296",oauth_nonce="ozERTeWsUnM",oauth_version="1.0",oauth_signature="rRneoL%2BLLiypEakCnuoEoLzmoLs%3D"'
     }
 
     response = requests.request("GET", url, headers=headers, data = payload)
     
     jdic = json.loads(response.text.encode('utf8'))
+    print(jdic)
     texts = list(map(lambda cont: cont["text"],jdic))
     print(texts)
     return texts
+
+def getRelatedTweets(tweets):
+    return tweets[0:1]
