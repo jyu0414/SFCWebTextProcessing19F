d7e94b8e22f322fcbd7881ad66ed7e69a0c6afa9
Yuji Sasaki <jyu0414@gmail.com>
2020-01-13 12:51:01 +0900

    fix twitter authentication error

--- gettweet.py
+++ gettweet.py
@@ -6,27 +6,23 @@ import json
 
 def getTweet():
     
-    tweets = getTweetTextsFor("Sankei_news")
-    #tweets.extend(getTweetTextsFor("asahi"))
-    #tweets.extend(getTweetTextsFor("Yomiuri_Online"))
+    tweets = getTweetTextsFor("Yomiuri_Online")
 
     return getRelatedTweets(tweets)
 
 def getTweetTextsFor(name):
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=Sankei_news&count=5"#.format(name)
-    print(url)
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={0}&count=5".format(name)
+
     payload = {}
     headers = {
-        'Authorization': 'OAuth oauth_consumer_key="JolGq4w2PqMWCfDr4szMLcupZ",oauth_token="1214413074179866625-XZsV1CVPd72h5wQ56zjNAMWycLKCA8",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1578448296",oauth_nonce="ozERTeWsUnM",oauth_version="1.0",oauth_signature="rRneoL%2BLLiypEakCnuoEoLzmoLs%3D"'
+    'Authorization': 'OAuth oauth_consumer_key="JolGq4w2PqMWCfDr4szMLcupZ",oauth_token="1214413074179866625-XZsV1CVPd72h5wQ56zjNAMWycLKCA8",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1578887051",oauth_nonce="OM5nRo29hB8",oauth_version="1.0",oauth_signature="pGeueAyxyo2LEAMf%2BNk%2BVYGyzXk%3D"'
     }
 
     response = requests.request("GET", url, headers=headers, data = payload)
     
     jdic = json.loads(response.text.encode('utf8'))
-    print(jdic)
     texts = list(map(lambda cont: cont["text"],jdic))
-    print(texts)
     return texts
 
 def getRelatedTweets(tweets):
-    return tweets[0:1]
+    return tweets[0:2]
